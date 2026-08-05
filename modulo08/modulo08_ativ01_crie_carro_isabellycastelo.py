914c422b19d681f30f850cf9312a181655b1a025
Aluno em Vocacao isabella sampaio <isabellycastelo02@gmail.com>
2026-08-05 11:39:26 -0300

    Update
    Update

---
+++ modulo08/modulo08_ativ01_crie_carro_isabellycastelo.py
@@ -0,0 +1,14 @@
+'''
+
+
+'''
+
+
+
+
+class carro:
+    def __init__(self, marca, modelo):
+        return f"marca: {self.marca} | modelo: {self.modelo}"
+
+    meu_carro = ("fiat", "uno")
+    print(meu_carro.exibir_info())
